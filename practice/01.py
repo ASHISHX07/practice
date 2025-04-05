b63c890d7e7d1a951ee4e85b8f9d561edc6c99eb
ASHX7 <ashishsindhav02@gmail.com>
2025-04-05 06:13:15 +0000

    container config done

--- practice/01.py
+++ practice/01.py
@@ -1,4 +1,4 @@
-if 0==0:
+if True:
     print("hello user, i'm front man\ni made this very easy and dumb code please don't laugh at it\n1. to get sum of two numbers enter '+'\n2. to subtract two numbers enter'-'\n3. to multiply two numbers enter '*'\n4. to divide two numbers enter'/'\n imp *please enter a valid input for results*")
 while True:
     try:
@@ -17,5 +17,31 @@ while True:
     except ValueError:
         print("Please enter a valid integer.")
 
+while True:
+    try:
+        c = input("Enter the operation sign (+, -, *, /): ")
+        
+        if c not in ['+', '-', '*', '/']:
+            raise ValueError("Invalid operation sign!")
+        
+        print(f"\nfirst value: {a}\nsecond value: {b}\noperation: {c}\n")
+        break
+        
+    except ValueError as e:
+        print(e)
 
+d = int(a+b)
+e = int(a-b)
+f = int(a*b)
+g = int(a/b)
+
+if c=='+':
+    print(f"{a} {c} {b} = {d}\nthanks using it")
+elif c=='-':
+    print(f"{a} {c} {b} = {e}\nthanks using it")
+elif c=='*':
+    print(f"{a} {c} {b} = {f}\nthanks using it")
+elif c=='/':
+    print(f"{a} {c} {b} = {g}\nthanks using it")
+    
 
